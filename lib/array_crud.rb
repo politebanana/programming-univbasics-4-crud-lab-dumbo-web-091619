0b299bc9d9b14b3abe109bfcfd27937e24310eb3
Christian Zambrana <chrislajas@gmail.com>
2019-07-10 03:26:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  << "hu"
+  
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift()
 end
 
 def remove_element_from_end_of_array(array)
